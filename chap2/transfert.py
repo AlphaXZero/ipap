0df5d39ea815f0b9134f42f3e60a4603d62ca500
gg <gg@gamil.com>
2025-01-22 19:48:07 +0000

    transfert end2

--- chap2/transfert.py
+++ chap2/transfert.py
@@ -1,6 +1,9 @@
-"""__author__ = Gvanderveen
+"""
+Écrivez un algorithme permettant de transférer les valeurs de trois variables a, b et c. Il
+ faut transférer a dans b, b dans c et c dans a, quels que soient les contenus préalables
+ de ces variables.
+__author__ = Gvanderveen
 __version__ = 0.1
-exercice 2.2 transfert
 """
 
 a = input("a ->")
