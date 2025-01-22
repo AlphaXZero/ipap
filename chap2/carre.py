51bf01032ba94e1a82e3a96c6fedc31b084c1052
gg <gg@gamil.com>
2025-01-22 19:54:19 +0000

    carre end

--- chap2/carre.py
+++ chap2/carre.py
@@ -1,6 +1,8 @@
-"""__author__ = Gvanderveen
+"""
+ Écrivez un algorithme qui demande un nombre à l’utilisateur, puis qui calcule le carré de
+ ce nombre.
+__author__ = Gvanderveen
 __version__ = 0.1
-exercice 2.3 carre
 """
 
 nbr = int(input("Entrez un nombre à mettre au carré "))
