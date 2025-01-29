65c5da3daf31286d363bfb7e3a1e5695b329d5bc
gg <gg@gamil.com>
2025-01-29 18:31:39 +0000

    exo 7 chap 4

--- chap4/maximum.py
+++ chap4/maximum.py
@@ -9,10 +9,11 @@ nbr1 = int(input("Entrez le premier nombre : "))
 nbr2 = int(input("Entrez le deuxième nombre : "))
 nbr3 = int(input("Entrez le troisième nombre : "))
 
-
-if nbr1 == 0 or nbr2 == 0:
-    print("Le résultat est nul")
-elif (nbr1 < 0) ^ (nbr2 < 0):
-    print("Le résultat est négatif")
+if nbr1 >= nbr2 and nbr1 >= nbr3:
+    print(nbr1)
+elif nbr2 >= nbr1 and nbr2 >= nbr3:
+    print(nbr2)
 else:
-    print("le résultat est positif")
+    print(nbr3)
+
+# print(max(nbr1,nbr2,nbr3))
