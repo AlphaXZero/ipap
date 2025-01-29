8afa028a5fac84c8fd9f0c4ec517fe6a4897003c
gg <gg@gamil.com>
2025-01-29 17:38:27 +0000

    exo1 chap 4

---
+++ chap4/positif_ou_negatif.py
@@ -0,0 +1,13 @@
+"""
+Écrivez un algorithme qui demande un nombre à l’utilisateur, puis qui l’informe si ce
+nombre est positif ou négatif. On considère que 0 est positif.
+__author__ = Gvanderveen
+__version__ = 0.1
+"""
+
+
+nbr = input("Entrez un nombre : ")
+if nbr >= 0:
+    print("Le nombre est positif")
+else:
+    print("Le nombre est négatif")
