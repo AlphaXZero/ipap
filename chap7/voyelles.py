4148701b6b1f885f70ea546e41e85e8bc46c7542
AlphaXZero <georgevanderveen@yahoo.fr>
2025-02-08 04:01:37 +0100

    exo voyelles

---
+++ chap7/voyelles.py
@@ -0,0 +1,9 @@
+"""
+Écrivez un algorithme qui déclare et remplisse un tableau contenant les 6 voyelles de
+l’alphabet latin.
+__author__ = Gvanderveen
+__version__ = 0.1
+"""
+
+tab = [i for i in "aeiouy"]
+print(tab)
