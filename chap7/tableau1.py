f2498ab6b53fc27224c03fcd818ef593e6b43085
AlphaXZero <georgevanderveen@yahoo.fr>
2025-02-08 04:01:25 +0100

    exo tableau1

---
+++ chap7/tableau1.py
@@ -0,0 +1,8 @@
+"""
+Écrivez un algorithme qui déclare et remplisse un tableau de 7 valeurs numériques en les
+mettant toutes à 0.
+__author__ = Gvanderveen
+__version__ = 0.1
+"""
+
+tab = [0 for i in range(7)]
