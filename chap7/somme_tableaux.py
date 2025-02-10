db7f4d08f600bea95b02108a0f16cc0318e32b2e
gg <georgevanderveen@yahoo.fr>
2025-02-10 18:39:07 +0100

    exo somme_tableaux

---
+++ chap7/somme_tableaux.py
@@ -0,0 +1,14 @@
+"""
+Écrivez  un  algorithme  constituant  un  tableau  à  partir  des  deux  tableaux  suivants.  Le
+nouveau tableau contiendra la somme des éléments des deux tableaux de départ
+__author__ = Gvanderveen
+__version__ = 0.1
+"""
+
+tab1 = [4,8,7,9,1,5,4,6]
+tab2 = [7,6,5,2,1,3,7,4]
+tab_summed = [0 for i in tab1]
+
+for i in range(len(tab1)):
+    tab_summed[i] = tab1[i]+tab2[i]
+print(tab_summed)
