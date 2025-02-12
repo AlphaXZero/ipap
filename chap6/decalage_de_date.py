5141afea0915be1fbe23bdbf12d8984b1a1a536e
gg <georgevanderveen@yahoo.fr>
2025-02-13 00:37:10 +0100

    error verif retour de monnaie

--- chap6/decalage_de_date.py
+++ chap6/decalage_de_date.py
@@ -18,14 +18,14 @@ day += nbr_days
 flag = False
 while(not flag):
     if month in (1, 3, 5, 7,8, 10, 12):
-        month_change += 1 if day > 31 else 0
+        divmod()
+
         if day > 31:
             day -= 31
             month = (month + 1) if month != 12 else 1
         else:
             flag = True
     elif month in (4, 6,9, 11):
-        month_change += 1 if day > 30 else 0
         if day > 30:
             day -= 30
             month = (month + 1) if month != 12 else 1
@@ -44,41 +44,7 @@ while(not flag):
                 month = (month + 1) if month != 12 else 1
             else:
                 flag = True
-    if month == 1:
+    if month == 1 and not flag:
         year +=1
-
-
-# if month in (1, 3, 5, 6, 7, 10, 12):
-#     if day > 31:
-#         print("invalid day")
-#         exit() 
-#     month_change = nbr_days // 31
-#     day = day + (nbr_days %31)
-# elif month in (4, 8, 9, 11):
-#     if day > 30:
-#         print("invalid day")
-#         exit()
-#     month_change = nbr_days // 30
-#     day = day + (nbr_days%30)
-# elif month == 2:
-#     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-#         if day > 29:
-#             print("invalid day")(nbr_days - month_change*31)
-#             exit()
-#         month_change = nbr_days // 29
-#         day = day + (nbr_days //29)
-#     else:
-#         if day > 28:
-#             print("invalid day")
-#             exit()
-#         month_change = nbr_days // 28
-#         day = day + (nbr_days //28)
-# else:
-#     print("invalid month")
-# if month_change:
-#     year_change = (month + month_change) // 12 
-#     month = (month % 12) + (month_change%12)
-# if year_change:
-#     year += year_change
 
 print(f"{day}/{month}/{year}")
--- chap6/retour_de_monnaie.py
+++ chap6/retour_de_monnaie.py
@@ -10,18 +10,24 @@ __version__ = 0.1
 total = 0
 usr_in = -1
 while usr_in != 0:
-    usr_in = int(input("Entre un prix ou 0 pour arrêter : "))
+    usr_in = int(input("Entrez un prix ou 0 pour arrêter : "))
     if usr_in >= 0:
         total += usr_in
     else:
         print("nombre invalide")
 
 print(f"Total : {total}€")
-usr_pay = int(input("Combien allez vous payer ? "))
-if usr_pay >= total:
-    usr_pay -= total
+while True:
+    usr_pay = int(input("Combien allez vous payer ? "))
+    if usr_pay >= total:
+        usr_pay -= total
+        break
+    else:
+        print("Erreur : entrez une somme supérieure ou égale au prix total")
+
 # j'ai déduis qu'on avait le droit car on avait utilisé les tuples pour l'exo sur le jour en plus
 # mais sinon j'aurai fais une grosse formule à base de tot//500, (tot%500)//200, ect
+print(f"{usr_pay}€ à rendre soit : ")
 for i in (500, 200, 100, 50, 20, 10, 5, 2, 1):
-    print(f"-{'billets' if i > 2 else 'pièces'} de {i}€ : {usr_pay // i}")
+    print(f"-{'billets' if i > 2 else 'pièces'} de {i}€ remis : {usr_pay // i}")
     usr_pay %= i
