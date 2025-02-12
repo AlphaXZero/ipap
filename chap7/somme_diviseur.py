acd6da38b1b8077a760cb4bb011be4f0a236ae74
gg <georgevanderveen@yahoo.fr>
2025-02-12 18:09:33 +0100

    exo somme div

---
+++ chap7/somme_diviseur.py
@@ -0,0 +1,18 @@
+"""
+Écrivez un algorithme permettant de stocker dans une liste tous les entiers positifs de
+3 chiffres tel que, pour chaque entier, la somme des chiffres est un diviseur du produit
+de ces chiffres. Ex: 514 est un nombre valide, car 5 + 1 + 4 = 10 est un diviseur de 5 × 1 ×
+4 = 20.
+__author__ = Gvanderveen
+__version__ = 0.1
+"""
+
+tab = []
+
+for i in range(100, 1000):
+    stri = str(i)
+    if (int(stri[0]) * int(stri[1]) * int(stri[2])) % (
+        int(stri[0]) + int(stri[1]) + int(stri[2])
+    ) == 0:
+        tab += [i]
+print(tab)
