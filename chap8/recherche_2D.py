1eb749ff30c27016ecddfab9f3ad251f04ae1061
gg <georgevanderveen@yahoo.fr>
2025-02-12 18:25:27 +0100

    exo recherche 2D

---
+++ chap8/recherche_2D.py
@@ -0,0 +1,17 @@
+"""
+Écrivez un algorithme qui recherche la plus grande valeur au sein d’un tableau à deux
+dimensions (12x8) préalablement rempli de valeurs numériques
+__author__ = Gvanderveen
+__version__ = 0.1
+"""
+
+tab = [[i for i in range(13)] for y in range(6)]
+
+maxi = -100000000
+
+for line in tab:
+    for row in line:
+        if row > maxi:
+            maxi = row
+
+print(maxi)
