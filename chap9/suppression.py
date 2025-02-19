75a91676b671035bdad9b93206e9caa044a79ab8
gg <georgevanderveen@yahoo.fr>
2025-02-19 21:21:16 +0100

    exo suppressino

---
+++ chap9/suppression.py
@@ -0,0 +1,16 @@
+"""
+Écrivez un algorithme qui permette à l’utilisateur de supprimer une valeur d’un tableau
+préalablement saisi. L’utilisateur donnera d’indice de la valeur qu’il souhaite supprimer.
+Attention, il ne s’agit pas de mettre la valeur à 0, mais bel et bien supprimer la case du
+tableau!. Il est interdit d’utiliser la fonction .remove() du tableau
+__author__ = Gvanderveen
+__version__ = 0.1
+"""
+
+tab = [1, 3, 10, 3, 2, 10]
+print(tab)
+
+ind_to_rem = int(input("Entrez l'indice de la liste que vous voulez retirer"))
+
+tab = tab[0:ind_to_rem] + tab[ind_to_rem + 1 : len(tab)]
+print(tab)
