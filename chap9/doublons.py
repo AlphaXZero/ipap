8cf25772a829f3e28d59128188820dd9e75c36a1
gg <georgevanderveen@yahoo.fr>
2025-03-10 18:29:00 +0100

    modif suppresion

---
+++ chap9/doublons.py
@@ -0,0 +1,24 @@
+"""
+Écrivez un algorithme qui permette de saisir les éléments d’un tableau et qui vérifie s’ils
+sont tous différents. L’algorithme affichera simplement s’il y a un ou plusieurs doublons
+ou aucun doublon le cas échéant
+__author__ = Gvanderveen
+__version__ = 0.1
+"""
+#TODO break ?
+is_typing = True
+tab=[]
+dubble = 0
+
+while(is_typing):
+    usr_in = input("Entre un nombre ou q pour arrêter : ")
+    if usr_in == "q":
+        is_typing = False
+    else:
+        if usr_in in tab:
+            dubble +=1
+        tab.append(usr_in)
+if dubble:
+    print(f"il y a {dubble} doublons")
+else:
+    print("il y a aucun doublons")
