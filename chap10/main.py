0a6bb17c2982d19841d43ad31889841ced1d673d
gg <georgevanderveen@yahoo.fr>
2025-03-12 18:51:11 +0100

    exo 3 et 4

--- chap10/main.py
+++ chap10/main.py
@@ -3,10 +3,11 @@ Les exos du chap 10
 __author__ = Gvanderveen
 __version__ = 0.1
 """
+
 import math
 
 
-def max1(a, b):
+def max1(a: int, b: int) -> int:
     """
     Écrivez une fonction qui demande deux paramètres entiers, puis qui renvoie le nombre
     maximum
@@ -16,7 +17,7 @@ def max1(a, b):
     return b
 
 
-def max2(*nbr):
+def max2_1(*nbr: int) -> int:
     """
     version avec args
     Écrivez  une  fonction  qui  demande  plusieurs  paramètres  entiers,  puis  qui  renvoie le
@@ -24,11 +25,12 @@ def max2(*nbr):
     """
     maxi = -math.inf
     for n in nbr:
-        if n > maxi: 
+        if n > maxi:
             maxi = n
     return maxi
 
-def max2(number_list):
+
+def max2_2(number_list: list) -> int:
     """
     version avec list
     Écrivez  une  fonction  qui  demande  plusieurs  paramètres  entiers,  puis  qui  renvoie le
@@ -36,6 +38,38 @@ def max2(number_list):
     """
     maxi = -math.inf
     for n in number_list:
-        if n > maxi: 
+        if n > maxi:
             maxi = n
     return maxi
+
+
+def fizzbuzz(nbr: int) -> str | int:
+    """
+    Écrivez une fonction nommée fizzbuzz prenant un nombre en paramètre. Cette fonction
+    renverra :
+    • FIZZ si le nombre est divisible par 3 ;
+    • BUZZ si le nombre est divisible par 5 ;
+    • FIZZBUZZ si le nombre est divisible par 3 et par 5 ;
+    • Dans les autres cas, la fonction renverra le nombre passé en paramètre
+    """
+    if nbr % 3 == 0:
+        if nbr % 5 == 0:
+            return "FIZZBUZZ"
+        return "FIZZ"
+    if nbr % 5 == 0:
+        return "BUZZ"
+    return nbr
+
+
+def permis(spd: int) -> str:
+    """
+    Écrivez une fonction qui vérifie une vitesse.
+    1. Si la vitesse est inférieure à 70km/h, elle renverra OK ;
+    2. Sinon, pour chaque tranche de 5km/h au-dessus de la limite, le conducteur perd 1 point
+    sur son permis. La fonction renverra le nombre de points perdus ;
+    3. Au-delà de 12 points perdus, la fonction renverra Permis suspendu.
+    """
+    if spd < 70:
+        return "OK"
+    pts = (spd - 70) // 5
+    return f" vous avez perdu {pts} points {', permis suspendu' if pts > 12 else ''}"
