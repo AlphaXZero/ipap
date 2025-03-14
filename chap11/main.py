0a250d91b7721750784c08045157f3658699250f
gg <georgevanderveen@yahoo.fr>
2025-03-14 18:03:52 +0100

    exo 11.1

--- chap11/main.py
+++ chap11/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from chap11!")
+
+def somme(nbr: int) -> int:
+    """
+    Écrivez  une  fonction  récursive  qui  renvoie  la  somme  des  nombres  entiers  jusqu’à  un
+    nombre préalablement entré. Exemple avec le nombre 5 : 1 + 2 + 3 + 4 + 5 = 15
+    """
+    if nbr==0:
+        return 0
+    return nbr + somme(nbr-1)
+
+
+
 
 
 if __name__ == "__main__":
-    main()
+    print(somme(5))
