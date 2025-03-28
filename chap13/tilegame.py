0182afa4e6060629cb8c91061eacc0a2537baa8c
gg <georgevanderveen@yahoo.fr>
2025-03-28 18:06:55 +0100

    ajout syllab

---
+++ chap13/tilegame.py
@@ -0,0 +1,15 @@
+"""
+Écrivez le jeu du 2048 en version console. Le but du jeu est d’afficher un tableau de
+4x4 cases avec des tuiles de couleur. Chaque tuile possède une valeur numérique entre
+2 et 2048 (toujours une puissance de deux). En appuyant sur une des flèches du clavier,
+on va pousser toutes les tuiles dans la direction indiquée. Si deux tuiles identiques se
+collisionnent  lors  de  la  compression,  elles  fusionnent  en  une  tuile  de  rang  supérieur.
+Après la compression, deux nouvelles tuiles apparaissent. Ces tuiles portent aléatoirement
+les numéros 2 ou 4.
+Le but est d’atteindre la tuile 2048.
+Analysez le problème pour en déduire les fonctions à écrire
+__author__ = Gvanderveen
+__version__ = 0.1
+"""
+
+import 
