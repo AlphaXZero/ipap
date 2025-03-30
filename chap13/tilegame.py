f3f976e77c790beb7fe9ff2bfab25c2ed72887be
gg <georgevanderveen@yahoo.fr>
2025-03-30 17:23:01 +0200

    COLORS + GEN INT

--- chap13/tilegame.py
+++ chap13/tilegame.py
@@ -14,24 +14,60 @@ __version__ = 0.1
 
 from keyboard import read_key
 from random import randrange
+from time import sleep
+from colorama import init, Fore, Back, Style
 
-colors = {2: "white", 4: ""}
+COLORS = {
+    0: Fore.WHITE,
+    2: Fore.CYAN,
+    4: Fore.GREEN,
+    8: Fore.YELLOW,
+    16: Fore.MAGENTA,
+    32: Fore.RED,
+    64: Back.WHITE,
+    128: Back.BLUE,
+    256: Back.GREEN,
+    512: Back.YELLOW,
+    1024: Back.MAGENTA,
+    2048: Back.RED,
+}
 
 
+# TODO demander sudo
+
+
+# TODO : correction bas + gagnant + random 2 cases + apparition après moove
 def show_game(board: list) -> None:
-    """ """
+    """
+    prints the board
+    """
     print("\n---------------")
     for i in board:
         for j in range(len(i)):
-            print(f"|{i[j]}| ", end="")
+            print(f"|{COLORS[i[j]]}{i[j]}{Style.RESET_ALL}| ", end="")
         print("\n---------------")
+
+
+def generate_2_4(board: list) -> list:
+    """
+    generate 2 int values between 2 and 4 in the board
+    """
+    index_zeros = [
+        (j, i) for j, row in enumerate(board) for i in range(len(row)) if row[i] == 0
+    ]
+    for i in range(2):
+        if not index_zeros:
+            return board
+        random_zero = index_zeros.pop(randrange(0, len(index_zeros)))
+        board[random_zero[0]][random_zero[1]] = randrange(2, 5, 2)
+    return board
 
 
 def create_board() -> list:
     """
     create a list with random entries of 2 or 4
     """
-    return [[randrange(2, 5, 2) for _ in range(4)] for _ in range(4)]
+    return generate_2_4([[0 for _ in range(4)] for _ in range(4)])
 
 
 def check_input() -> int:
@@ -42,7 +78,7 @@ def check_input() -> int:
     3 -> down
     4 -> left
     """
-    print("Choisisez une direction")
+    print("Choisisez une direction ou x pour arrêter")
     usr_choice = read_key()
     print(usr_choice)
     if usr_choice in ("up", "z", "Z"):
@@ -53,13 +89,14 @@ def check_input() -> int:
         return 3
     if usr_choice in ("left", "q", "Q"):
         return 4
+    if usr_choice == "x":
+        exit()
 
 
 def do_merge(board: list, direction: int, vertical=0) -> list:
     """
     merge all the adjacent number in the desired direction
     """
-    # TODO : une fonction de plus pour enlever répétion ?
     if direction in (2, 4):
         board = [i[::-1] for i in board] if direction == 2 else board
         for i in range(len(board)):
@@ -67,8 +104,8 @@ def do_merge(board: list, direction: int, vertical=0) -> list:
                 if board[i][j + 1] == board[i][j]:
                     board[i][j] *= 2
                     board[i][j + 1] = 0
-        board = rotate_board(board) if vertical else board
-        return board if direction == 4 or vertical else [i[::-1] for i in board]
+        board = board if direction == 4 else [i[::-1] for i in board]
+        return rotate_board(board) if vertical else board
     if direction == 1:
         return do_merge(rotate_board(board), 4, 1)
     return do_merge(rotate_board(board), 2, 1)
@@ -106,12 +143,14 @@ def main() -> None:
     """
     main function of the game
     """
+    init()
     board = create_board()
     show_game(board)
     while 2048 not in board:
         usr_in = check_input()
         board = cleanse_zero(do_merge(board, usr_in), usr_in)
         show_game(board)
+        sleep(0.4)
 
 
 if __name__ == "__main__":
